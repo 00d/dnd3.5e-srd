bb6e9bba94ea1433253741cef05a73d82468c414
Kate Korsaro <kate.korsaro@gmail.com>
2025-03-14 14:45:51 +0100

    adding columns to output

--- scripts/convert2md.sh
+++ scripts/convert2md.sh
@@ -2,7 +2,7 @@
 
 for file in ../rtf/*; do
   echo "Processing file $file"
-  pandoc -t markdown-multiline_tables "$file" | tee "./${file%.rtf}.md"
+  pandoc -t markdown --columns 10000 "$file" | tee "./${file%.rtf}.md"
 done;
 
-mv ../rtf/*.md ../md/
+mv ../rtf/*.md ../001-md/
