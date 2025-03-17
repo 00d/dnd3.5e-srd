40e93cb37747ac422ca246dfaade01ebd760511c
Kate Korsaro <kate.korsaro@gmail.com>
2025-03-17 07:11:36 +0100

    restructuring folders

--- scripts/convert2md.sh
+++ scripts/convert2md.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-for file in ../rtf/*; do
+for file in ../01-original-rtf/*; do
   echo "Processing file $file"
   pandoc -t markdown --columns 10000 "$file" | tee "./${file%.rtf}.md"
 done;
 
-mv ../rtf/*.md ../001-md/
+mv ../rtf/*.md ../01-conversion/
