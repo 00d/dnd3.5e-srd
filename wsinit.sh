a25b4cdd81f3f45b0603f671393e91d16331cc86
Kate Korsaro <kate.korsaro@gmail.com>
2025-03-31 11:22:34 +0200

    updating workspace script

--- wsinit.sh
+++ wsinit.sh
@@ -1,17 +1,23 @@
 #!/bin/bash
 
+
 echo "Starting alacritty"
 alacritty --working-directory . &
 sleep 0.3
-echo "Starting glow"
-alacritty --working-directory . -o font.size=16.0 -e glow . &
-sleep 0.3
+
+# echo "Starting glow"
+# alacritty --working-directory . -o font.size=16.0 -e glow . &
+# sleep 0.3
+
 echo "Starting lazygit"
 alacritty --working-directory . -e lazygit &
 sleep 0.3
+
 echo "Starting yazi"
 alacritty --working-directory . -e yazi &
 sleep 0.3
-echo "Starting nvim"
-alacritty --working-directory . -o font.size=15.0 -e nvim &
+
+# echo "Starting nvim"
+# alacritty --working-directory . -o font.size=15.0 -e nvim &
+
 echo "done"
